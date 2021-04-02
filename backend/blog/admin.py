d120dfa60640f649f3ccb15c7f88710801dd1a11
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-02 20:35:50 +0430

    ArticleAdmin - CategoryAdmin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Article, Category
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created', 'cover')
+    list_filter = ('title', 'created', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
