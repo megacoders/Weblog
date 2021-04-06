25cd1f4a089dc211e6294f7a3729e47ac8131814
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-06 23:30:50 +0430

    feat: [200~IsSuperUser

--- backend/blog/permissions.py
+++ backend/blog/permissions.py
@@ -1,10 +1,27 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
 class IsStaffOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS or
             request.user and
             request.user.is_staff
+        )
+
+
+class IsSuperUserOrStaffReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            # get access to authors readonly
+            request.method in SAFE_METHODS and
+            request.user and
+            request.user.is_staff or
+            # get access to superuser full
+            request.user and
+            request.user.is_superuser
         )
