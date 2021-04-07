33e78e111b9cb73c150edf925d1741d0db14b305
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-04-07 13:22:40 +0430

    feat: added custom scrollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import SimpleBar from 'simplebar-react';
 import { render } from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './containers/App';
 import RtlProvider from './components/Chakra/RtlProvider';
 
+import 'simplebar/dist/simplebar.min.css';
 import './assets/styles/fonts.css';
 import './assets/styles/index.css';
 
 render(
-  <ChakraProvider>
-    <RtlProvider>
-      <App />
-    </RtlProvider>
-  </ChakraProvider>,
+  <SimpleBar style={{ maxHeight: '100vh' }}>
+    <ChakraProvider>
+      <RtlProvider>
+        <App />
+      </RtlProvider>
+    </ChakraProvider>
+  </SimpleBar>,
   document.getElementById('app')
 );
