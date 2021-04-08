cd0acc5307dfeb3a1199a235ef5e9e804038ab9b
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-08 22:25:39 +0430

    feat: IsAuthorOrReadOnly permission

--- backend/blog/permissions.py
+++ backend/blog/permissions.py
@@ -25,3 +25,17 @@ class IsSuperUserOrStaffReadOnly(BasePermission):
             request.user and
             request.user.is_superuser
         )
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return bool(
+            # get access to superuser
+            request.user.is_authenticated and
+            request.user.is_superuser or
+            # get access to author of objet
+            obj.author == request.user
+        )
