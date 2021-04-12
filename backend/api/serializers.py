8797053caf32f341073a351b55b5891e935544d6
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-12 11:07:41 +0430

    feat: AuthorSerializer(more user data)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Article, Category
+from accounts.models import User
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'id', 'is_staff')
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+
     class Meta:
         model = Article
         fields = ['author', 'category', 'title', 'description', 'cover', 'created', 'updated']
